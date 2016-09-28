627b549d00a52d6aa9ce8cd3c477b6a4b5c8e7fc
Paul G. Frey, Jr <paulgfrey@gmail.com>
2016-09-27 22:49:04 -0400

    checkpoint commmit

--- PlexPurge/src/com/pf/guidebox/GuideBoxAPIException.java
+++ PlexPurge/src/com/pf/guidebox/GuideBoxAPIException.java
@@ -3,28 +3,24 @@ package com.pf.guidebox;
 public class GuideBoxAPIException extends Exception {
 
 	public GuideBoxAPIException() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
 	public GuideBoxAPIException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GuideBoxAPIException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GuideBoxAPIException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GuideBoxAPIException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
